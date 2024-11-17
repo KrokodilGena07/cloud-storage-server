2e1f2f152c438147a44136737eb90c6e4c7d894c
KrokodilGena07 <pasha.fenev@gmail.com>
2024-11-17 23:23:47 +0300

    made auth middleware

--- src/domain/users/usersRouter.js
+++ src/domain/users/usersRouter.js
@@ -2,12 +2,13 @@ const Router = require('express');
 const usersController = require('./usersController');
 const userPasswordValidator = require('./validators/userPasswordValidator');
 const userValidator = require('../../validators/userValidator');
+const authMiddleware = require('../../middlewares/authMiddleware');
 
 const usersRouter = new Router();
 
-usersRouter.get('/', usersController.checkUserPassword);
-usersRouter.put('/', ...userValidator, usersController.updateUser);
-usersRouter.put('/password', ...userPasswordValidator, usersController.updateUserPassword);
-usersRouter.delete('/:id', usersController.deleteUser);
+usersRouter.get('/', authMiddleware, usersController.checkUserPassword);
+usersRouter.put('/', authMiddleware, ...userValidator, usersController.updateUser);
+usersRouter.put('/password', authMiddleware, ...userPasswordValidator, usersController.updateUserPassword);
+usersRouter.delete('/:id', authMiddleware, usersController.deleteUser);
 
 module.exports = usersRouter;
